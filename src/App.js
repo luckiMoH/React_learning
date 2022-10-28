556c0cc40e8e5267decb3960b85a230e391d5f91
LuckiMoH <mat.maj1992@gmail.com>
2022-10-28 09:53:09 +0200

    First mini project with react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import "./scss/main.scss"
 
 
 const App = () => {
     return (
-        <>
-
-        </>
+        <section>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
     );
 };
+
+const Book = () => {
+    return (
+        <article>
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    )
+}
+const Image = () => {
+    return (
+        <img src="https://images-na.ssl-images-amazon.com/images/I/71PNGYHykrL._AC_UL600_SR600,400_.jpg" alt="book"/>
+    )
+}
+const Title = () => {
+    return (
+        <h1>It Starts with Us: A Novel (It Ends with Us)</h1>
+    )
+}
+const Author = () => {
+    return (
+        <h2>Colleen Hoover</h2>
+    )
+}
 
 export default App;
