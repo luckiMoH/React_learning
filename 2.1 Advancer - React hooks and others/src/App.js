0b620459f029fa4159ba624e35ac3772f190cdf1
LuckiMoH <mat.maj1992@gmail.com>
2022-11-02 23:34:42 +0100

    props drilling

--- 2.1 Advancer - React hooks and others/src/App.js
+++ 2.1 Advancer - React hooks and others/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import PropDrilling from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 
 
 function App() {
   return (
     <div className='container'>
-
+      <PropDrilling />
+      <hr />
+      <br />
     </div>
   )
 }
