e3de350a84fb6c2664aedd38b9835b6e05d98c6f
LuckiMoH <mat.maj1992@gmail.com>
2022-11-03 13:58:41 +0100

    useFetch

--- 2.1 Advancer - React hooks and others/src/App.js
+++ 2.1 Advancer - React hooks and others/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropDrilling from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 import ContextAPI from "./tutorial/8-useContext/setup/1-context-api";
+import FetchExample from "./tutorial/9-custom-hooks/setup/1-fetch-example";
+import useFetch from "./tutorial/9-custom-hooks/setup/2-useFetch";
 
 
 function App() {
@@ -10,6 +12,9 @@ function App() {
       <hr />
       <br />
         <ContextAPI />
+        <hr />
+        <br />
+        <FetchExample />
         <hr />
         <br />
     </div>
