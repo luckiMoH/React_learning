2d6a286af3d14270faeaf1c3d2fcd1d7980b43af
LuckiMoH <mat.maj1992@gmail.com>
2022-11-06 13:39:11 +0100

    Axios HTTP Library start

---
+++ 4. Axios HTTP Library/src/components/StyledNavbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link, NavLink, Outlet} from "react-router-dom";
+
+const StyledNavbar = () => {
+    return (
+        <nav className='navbar'>
+            <NavLink to='/' className={({isActive}) => {
+                return isActive ? 'link active' : 'link'
+            }}>Home</NavLink>
+            <NavLink to='/about' className={({isActive}) => {
+                return isActive ? 'link active' : 'link'
+            }}>About</NavLink>
+            <NavLink to='/products' className={({isActive}) => {
+                return isActive ? 'link active' : 'link'
+            }}>Products</NavLink>
+            <NavLink to='/login' className={({isActive}) => {
+                return isActive ? 'link active' : 'link'
+            }}>Login</NavLink>
+        </nav>
+    );
+};
+
+export default StyledNavbar;
